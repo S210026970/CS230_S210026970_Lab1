7f00a0aa179b11a935a4aa3482d1f086743b0ebf
SALMAN ALBISHI <114346928+S210026970@users.noreply.github.com>
2022-09-28 11:00:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main (String [] args){
+    int = 60;
+    String StudentGrade = 70;
+    if (StudentGrade>=60);
+    System.out.println("Pass");
+    
+  }
+
+}
